9d97d857695023a4a9afaa9ef039c0231be20bdf
Alex Yanitskiy <demo.alex@gmail.com>
2021-11-05 10:40:27 +0300

    init events

--- tasks/tasks/models.py
+++ tasks/tasks/models.py
@@ -22,5 +22,71 @@ class Task(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(auto_now=True, null=True, db_index=True, editable=False)
 
+    __original_status = None
+
+    def __init__(self, *args, **kwargs):
+        super(Task, self).__init__(*args, **kwargs)
+        self.__original_status = self.status
+
+    def save(self, *args, **kwargs):
+        # new task
+        if not self.id:
+            # produce CUD event
+            event = {
+                'event_name': 'TaskCreated',
+                'event_type': 'CUD',
+                'data': {
+                    'public_id': str(self.public_id),
+                    'reporter': str(self.reporter.id),
+                    'assignee': str(self.assignee.id),
+                    'description': self.description,
+                    'status': self.status
+                }
+            }
+            topic = 'tasks-stream'
+            # produce CUD event END
+        else:
+            # produce CUD event
+            event = {
+                'event_name': 'TaskUpdated',
+                'event_type': 'CUD',
+                'data': {
+                    'public_id': str(self.public_id),
+                    'reporter': str(self.reporter.id),
+                    'assignee': str(self.assignee.id),
+                    'description': self.description,
+                    'status': self.status
+                }
+            }
+            topic = 'tasks-stream'
+            # produce CUD event END
+        super(Task, self).save(*args, **kwargs)
+        if self.status != self.__original_status:
+            # produce Business event
+            event = {
+                'event_name': 'TaskRoleChanged',
+                'event_type': 'business',
+                'data': {
+                    'public_id': str(self.public_id),
+                    'new_status': self.status,
+                    'original_status': self.__original_status
+                }
+            }
+            topic = 'tasks'
+            # produce Business event END
+
+    def delete(self, *args, **kwargs):
+        super(Task, self).delete(*args, **kwargs)
+        # produce CUD event
+        event = {
+            'event_name': 'TaskDeleted',
+            'event_type': 'CUD',
+            'data': {
+                'public_id': str(self.public_id)
+            }
+        }
+        topic = 'tasks-stream'
+        # produce CUD event END
+
     def __str__(self):
         return f'#{self.id}: {self.status}: {truncatechars(self.description, 30)}'
